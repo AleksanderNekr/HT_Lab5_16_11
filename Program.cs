695e8cfbb9e2438031a24ababa708f40fab769ad
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2021-11-19 07:58:47 +0500

    Created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HT_Lab5_16_11
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
