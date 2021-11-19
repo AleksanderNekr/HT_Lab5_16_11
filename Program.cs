e75ccb9cbd4773dd431894fbba5e06a21c683a26
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2021-11-19 23:22:21 +0500

    to expression body

--- Program.cs
+++ Program.cs
@@ -2,8 +2,5 @@
 
 internal static class Program
 {
-    private static void Main()
-    {
-        Console.WriteLine("Hello, world!");
-    }
+    private static void Main() => Console.WriteLine("Hello, world!");
 }
