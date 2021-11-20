a4257f439e5649d66a7da3d496c6a68f7460ab8a
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2021-11-20 21:30:53 +0500

    Some input changes

--- Program.cs
+++ Program.cs
@@ -7,38 +7,34 @@ internal static class MyArray
     /// <summary>
     ///     Ввод числа типа <see cref="T:System.Int32" />
     /// </summary>
-    private static int ReadInt()
+    private static void ReadInt(out int intNum)
     {
         bool isConvert;
-        int  intNum;
         do
         {
             isConvert = int.TryParse(Console.ReadLine(), out intNum);
             if (!isConvert)
                 Console.Write("ОШИБКА! Введено нецелое число!\nВведите заново: ");
         } while (!isConvert);
-
-        return intNum;
     }
 
     /// <summary>
     ///     Ручной ввод элементов массива
     /// </summary>
+    /// <param name="arrayInts">Выходной массив <see cref="T:System.Int32" /> значений</param>
     /// <param name="size">Размер массива</param>
-    /// <returns>Массив <see cref="T:System.Int32" /> значений</returns>
-    internal static int[] ReadArray(uint size)
+    internal static void ReadArray(out int[] arrayInts, uint size)
     {
-        var arrayInts = new int[size];
+        arrayInts = new int[size];
         for (var i = 0; i < size; i++)
         {
             Console.Write($"Введите элемент №{i + 1}: ");
-            arrayInts[i] = ReadInt();
+            ReadInt(out arrayInts[i]);
         }
 
         Console.WriteLine("Массив успешно сформирован");
         if (size == 0)
             Console.WriteLine("Массив не содержит элементов");
-        return arrayInts;
     }
 }
 
@@ -46,7 +42,8 @@ internal static class Program
 {
     private static void Main()
     {
-        var arr = MyArray.ReadArray(5);
-        foreach (var variable in arr) Console.Write(variable + " ");
+        MyArray.ReadArray(out var arr, 5);
+        foreach (var variable in arr)
+            Console.Write(variable + " ");
     }
 }
