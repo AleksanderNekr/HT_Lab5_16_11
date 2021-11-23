b8b29e88b76806049a7aa0d4a62ad499d97d3976
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2021-11-23 16:32:21 +0500

    Implicit variable types

--- Program.cs
+++ Program.cs
@@ -20,10 +20,13 @@ internal static class Program
 
     private static void Main()
     {
-        Read(out var arr, 5, 4);
+        // Тип значений, допускающий значение NULL, или T?,
+        // представляет все значения своего базового типа значения T,
+        // а также дополнительное значение NULL
+        Generate(out int[,]? arr, 7, 7);
         Write(arr);
         Console.WriteLine();
-        arr = arr.DeleteColWithZero();
+        arr = arr.DeleteColumnContainsZero();
         Write(arr);
     }
 
@@ -37,7 +40,7 @@ internal static class Program
     private static void Read(out int[] arrayInts, uint size)
     {
         arrayInts = new int[size];
-        for (var i = 0; i < size; i++)
+        for (int i = 0; i < size; i++)
         {
             Console.Write($"Введите элемент №{i + 1}: ");
             ReadInt(out arrayInts[i]);
@@ -58,8 +61,8 @@ internal static class Program
     {
         arrayInts = new int[sizeArray];
 
-        var generator = new Random();
-        for (var i = 0; i < sizeArray; i++)
+        Random generator = new();
+        for (int i = 0; i < sizeArray; i++)
             arrayInts[i] = generator.Next(-100, 101);
 
         Console.WriteLine("Массив успешно сформирован");
@@ -75,7 +78,7 @@ internal static class Program
     {
         if (arrayInts.Length > 0)
         {
-            foreach (var variable in arrayInts)
+            foreach (int variable in arrayInts)
                 Console.Write(variable + " ");
             Console.WriteLine();
         }
@@ -109,7 +112,7 @@ internal static class Program
     /// <param name="index">Индекс, на место которого начинать вставлять массив (по умолчанию – 0)</param>
     private static int[] Append(this int[] arrayInts, int[] arrayIntsAdd, int index = 0)
     {
-        foreach (var element in arrayIntsAdd)
+        foreach (int element in arrayIntsAdd)
             arrayInts = arrayInts.Append(element, index);
         return arrayInts;
     }
@@ -127,8 +130,8 @@ internal static class Program
     private static void Read(out int[,] matrInts, uint sizeRow, uint sizeColumn)
     {
         matrInts = new int[sizeRow, sizeColumn];
-        for (var i = 0; i < sizeRow; i++)
-            for (var j = 0; j < sizeColumn; j++)
+        for (int i = 0; i < sizeRow; i++)
+            for (int j = 0; j < sizeColumn; j++)
             {
                 Console.Write($"Введите элемент в ячейке [{i + 1}, {j + 1}]: ");
                 ReadInt(out matrInts[i, j]);
@@ -151,10 +154,10 @@ internal static class Program
     {
         matrInts = new int[sizeRow, sizeColumn];
 
-        var generator = new Random();
-        for (var i = 0; i < sizeRow; i++)
-            for (var j = 0; j < sizeColumn; j++)
-                matrInts[i, j] = generator.Next(-5, 5);
+        Random generator = new();
+        for (int i = 0; i < sizeRow; i++)
+            for (int j = 0; j < sizeColumn; j++)
+                matrInts[i, j] = generator.Next(-2, 2);
 
         Console.WriteLine("Матрица успешно сформирована");
         if (matrInts.Length == 0)
@@ -168,9 +171,9 @@ internal static class Program
     private static void Write(int[,] matrInts)
     {
         if (matrInts.Length > 0)
-            for (var i = 0; i < matrInts.GetLength(0); i++)
+            for (int i = 0; i < matrInts.GetLength(0); i++)
             {
-                for (var j = 0; j < matrInts.GetLength(1); j++)
+                for (int j = 0; j < matrInts.GetLength(1); j++)
                     Console.Write($"{matrInts[i, j],4} ");
 
                 Console.WriteLine();
@@ -180,31 +183,40 @@ internal static class Program
     }
 
     /// <summary>
-    ///     Удаляет из двумерного массива столбец с заданным индексом
+    ///     Удаляет из двумерного массива <see cref="T:System.Int32" /> значений
+    ///     столбец с заданным индексом
     /// </summary>
     /// <param name="matrInts">Двумерный массив</param>
     /// <param name="indexColumn">Индекс удаляемого столбца</param>
     private static int[,] DeleteColumn(this int[,] matrInts, int indexColumn)
     {
-        var resMatrInts = new int[matrInts.GetLength(0), matrInts.GetLength(1) - 1];
-        for (var j = 0; j < matrInts.GetLength(1); j++)
+        int[,] resMatrInts = new int[matrInts.GetLength(0), matrInts.GetLength(1) - 1];
+        for (int j = 0; j < matrInts.GetLength(1); j++)
             if (j < indexColumn)
-                for (var i = 0; i < matrInts.GetLength(0); i++)
+                for (int i = 0; i < matrInts.GetLength(0); i++)
                     resMatrInts[i, j] = matrInts[i, j];
             else if (j > indexColumn)
-                for (var i = 0; i < matrInts.GetLength(0); i++)
+                for (int i = 0; i < matrInts.GetLength(0); i++)
                     resMatrInts[i, j - 1] = matrInts[i, j];
         return resMatrInts;
     }
 
-    private static int[,] DeleteColWithZero(this int[,] matrInts)
+    /// <summary>
+    ///     Удаляет из двумерного массива <see cref="T:System.Int32" /> значений
+    ///     все столбцы, содержащие хотя бы один 0
+    /// </summary>
+    /// <param name="matrInts">Двумерный массив</param>
+    private static int[,] DeleteColumnContainsZero(this int[,] matrInts)
     {
-        for (var j = 0; j < matrInts.GetLength(1); j++)
-            for (var i = 0; i < matrInts.GetLength(0); i++)
+        for (int j = 0; j < matrInts.GetLength(1); j++)
+            for (int i = 0; i < matrInts.GetLength(0); i++)
             {
                 if (j >= matrInts.GetLength(1) || matrInts[i, j] != 0)
                     continue;
                 matrInts = matrInts.DeleteColumn(j);
+
+                // Приравниваем -1, так как в цикле for призойдет инкремент на 1,
+                // а нужно проверять с элемента с индексом 0
                 i = -1;
             }
 
